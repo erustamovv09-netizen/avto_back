0c104610787d4e16ba5f1da8fdfb6f5a2bbfb73b
erustamovv09-netizen <e.rustamovv.09@gmail.com>
2026-03-16 15:40:14 -0700

    Mashinalar modeli maydonlari to'liq bazaga tushirildi

--- sotuvchi/models.py
+++ sotuvchi/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
 class mashinalar(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=50)
     brand = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
-    # Pastdagi qatorlarga qavslar va max_length qo'shildi:
-    transmission = models.CharField(max_length=50) 
-    engine_volume = models.IntegerField()
-    year = models.IntegerField()
-    fuel_type = models.CharField(max_length=50)
-    turi = models.CharField(max_length=100)
+    
+    # Yangi maydonlar (default qiymatlari bilan):
+    transmission = models.CharField(max_length=50, default="Mexanika")
+    engine_volume = models.IntegerField(default=0)
+    year = models.IntegerField(default=2024)
+    fuel_type = models.CharField(max_length=50, default="Benzin")
+    turi = models.CharField(max_length=100, default="Sedan")
